ad6d4bf49bf3335a423aae03e8f7be62310cbe05
shadman-a <shadman@outlook.com>
2020-07-27 17:27:08 -0400

    frontend

--- flatiron-match-frontend/src/index.js
+++ flatiron-match-frontend/src/index.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll('.memory-card');
+    const url = "http://localhost:3000/cards"
 
+    function fetchCards() {
+        fetch(url)
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+    }
+
+
+    function flipCard() {
+        this.classList.toggle('flip');
+    }
+
+    cards.forEach(card => card.addEventListener('click', flipCard));
     
-
+    fetchCards()
 })   
