d2908c74ba075bf758a1f143d9c6095d73c5a860
shadman-a <shadman@outlook.com>
2020-07-27 13:39:40 -0400

    generate user

--- flatiron-match-backend/app/controllers/users_controller.rb
+++ flatiron-match-backend/app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+    
+    def index
+        users = User.all
+
+        render json:users 
+    end
+
+    def show
+        user = User.find(params[:id])
+    end
+
+    def create
+        user = User.new(user_params)
+
+        render json:users 
+    end
+
+    def update
+        user = User.find(params[:id])
+
+        user.update(user_params)
+
+        render json:users 
+    end
+
+    def edit
+        user = User.find(params[:id])
+    end
+
+    def destroy
+        user = User.find
+
+        user.destroy 
+        render
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
+
 end
