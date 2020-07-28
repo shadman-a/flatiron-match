9c7c5ac4a8873928a43a5a7fb7b5437909663330
shadman-a <shadman@outlook.com>
2020-07-28 15:57:06 -0400

    85

--- flatiron-match-frontend/src/index.js
+++ flatiron-match-frontend/src/index.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const memoryCard = document.querySelector('.memory-card');
     const url = "http://localhost:3000/cards"
     
 
@@ -14,33 +13,84 @@ document.addEventListener("DOMContentLoaded", () => {
     const frontFace = document.getElementById('memory-game')
     const div = document.createElement('class')
     div.className = 'memory-card'
+    div.id = card.id
     div.innerHTML = `      
-    <img class="front-face" id= ${card.id} src=${card.cardside}>
-    <img class="back-face" src="https://cdn.bootcamprankings.com/spai/w_210+q_lossy+ret_img+to_webp/https://bootcamprankings.com/wp-content/uploads/2019/10/36776548_1553913434714928_4773274533622710272_n.png" alt="Memory Card">
+        <img class="front-face" src=${card.cardside}>
+        <img class="back-face" src="https://cdn.bootcamprankings.com/spai/w_210+q_lossy+ret_img+to_webp/https://bootcamprankings.com/wp-content/uploads/2019/10/36776548_1553913434714928_4773274533622710272_n.png" alt="Memory Card">
     `
     frontFace.append(div)
+    }   
+
+    let hasFlippedCard = false;
+    let lockBoard = false;
+    let firstCard, secondCard;
+
+    function flipCard(card) { 
+        if (lockBoard) return;
+        if (card === firstCard) return;
+        card.classList.add('flip');
+    
+        
+        if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = card;
+            return;
+        }
+
+        secondCard = card;
+
+        checkForMatch();
     }
 
-
-    function flipCard(card) {
-        card.toggle('flip');     
+    function checkForMatch() {
+        if (firstCard.id === secondCard.id) {
+            disableCards();
+        return;
     }
 
+    unflipCards();
+    }
+
+    function disableCards() {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+        resetBoard();
+    }
+
+    
+
+    function unflipCards() {
+        lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);
+    }
+                
+    function resetBoard() {
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    }
+
+    const memoryCard = document.querySelectorAll('.memory-card');
+
+    (function shuffle() {
+        memoryCard.forEach(card => {
+          let ramdomPos = Math.floor(Math.random() * 16);
+          card.style.order = ramdomPos;
+        });
+      })();
+
+
+
+    //listners
     document.addEventListener('click', function(e) {
         if(e.target.className = ('memory-card')){
-        flipCard(e.target.classList)
+        flipCard(e.target)
         }
     })
 
-    document.addEventListener('click', function(e) {
-        if(e.target.innerText = ('memory-card'))
-        flipCard(e.target.className)
-        console.log(e.target.className);
-        
-    })
-/// we need to store the image to a div
 
-
-    
     fetchCards()
 })   
